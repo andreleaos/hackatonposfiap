23111ea76d054e209ac3692793b1adc7362c0d4d
Liandro <liandro.freire@gmail.com>
2024-03-16 14:12:05 -0300

    Ajuste para service save Infos

--- hackatonposfiap/src/commons/hackatonposfiap.services/ExtracaoImagensService.cs
+++ hackatonposfiap/src/commons/hackatonposfiap.services/ExtracaoImagensService.cs
@@ -13,22 +13,29 @@ namespace hackatonposfiap.services
 {
     public class ExtracaoImagensService : IExtracaoImagensService
     {
+
         private readonly IGerenciadorImagemRepository _imagemRepository;
         private readonly IConfiguration _configuration;
-        public ExtracaoImagensService(IGerenciadorImagemRepository imagemRepository, IConfiguration configuration)
+        private readonly IGerenciadorVideoService _gerenciadorVideoService;
+
+        public ExtracaoImagensService(IGerenciadorImagemRepository imagemRepository, IConfiguration configuration, IGerenciadorVideoService gerenciadorVideoService)
         {
             _imagemRepository = imagemRepository;
             _configuration = configuration;
+            _gerenciadorVideoService = gerenciadorVideoService;
         }
+
+
         public Task Processar(GerenciadorVideoDto gerenciadorVideoDto)
         {
             var diretorioBase = CriarDiretorioInicial();
             var outputFolder = diretorioBase[0];
             string destinationZipFilePath = diretorioBase[1];
 
-
             foreach (GerenciadorVideoItemDto video in gerenciadorVideoDto.Arquivos)
             {
+                SaveInfoVideo(video);
+
                 Directory.CreateDirectory(outputFolder);
 
                 var videoInfo = FFProbe.Analyse(video.CaminhoArquivo);
@@ -42,24 +49,39 @@ namespace hackatonposfiap.services
                     FFMpeg.Snapshot(video.CaminhoArquivo, outputPath, new Size(1920, 1080), currentTime);
                 }
 
-                var listaImagens = Directory.GetFiles(outputFolder);
-
-                foreach (var imagem in listaImagens)
-                {
-                    var imagemItem = new GerenciadorImagemItem();
-
-                    imagemItem.CaminhoArquivo = outputFolder + @$"/{imagemItem}";
-                    imagemItem.NomeArquivo = imagem;
-
-                    _imagemRepository.Create(imagemItem);
-                }
-
+                SaveInfoImages(outputFolder);
 
                 ZipFile.CreateFromDirectory(outputFolder, destinationZipFilePath);
 
             }
 
             return Task.CompletedTask;
+        }
+
+        private void SaveInfoVideo(GerenciadorVideoItemDto video)
+        {
+            var newVideo = new GerenciadorVideoItem()
+            {
+                CaminhoArquivo = video.CaminhoArquivo,
+                NomeArquivo = video.NomeArquivo,
+                Intervalo = video.Intervalo
+            };
+            _gerenciadorVideoService.Create(newVideo);
+        }
+
+        private void SaveInfoImages(string outputFolder)
+        {
+            var listaImagens = Directory.GetFiles(outputFolder);
+
+            foreach (var imagem in listaImagens)
+            {
+                var imagemItem = new GerenciadorImagemItem();
+
+                imagemItem.CaminhoArquivo = outputFolder + @$"/{imagemItem}";
+                imagemItem.NomeArquivo = imagem;
+
+                _imagemRepository.Create(imagemItem);
+            }
         }
 
         private string[] CriarDiretorioInicial()
@@ -81,7 +103,6 @@ namespace hackatonposfiap.services
 
             return directorys;
         }
-
 
 
 
