6a2241ae7916eb5a9ca93c0daa44e67d69490ae2
andreleaos <andreleaos@gmail.com>
2024-03-16 10:35:01 -0300

    atualizacao

---
+++ hackatonposfiap/src/web/hackatonposfiap.web/Controllers/GerenciadorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hackatonposfiap.web.Controllers
+{
+    public class GerenciadorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
