bddea39ce117e9a10c402217861b22ac7ae77ab8
andreleaos <andreleaos@gmail.com>
2024-03-16 15:35:03 -0300

    ajustes implementacao

--- hackatonposfiap/src/queue/hackatonposfiap.producer/Program.cs
+++ hackatonposfiap/src/queue/hackatonposfiap.producer/Program.cs
@@ -4,10 +4,17 @@ namespace hackatonposfiap.producer
     {
         public static void Main(string[] args)
         {
+            IConfiguration configuration = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                  .Build();
+
             IHost host = Host.CreateDefaultBuilder(args)
                 .ConfigureServices(services =>
                 {
+                    services.AddSingleton(configuration);
                     services.AddHostedService<Worker>();
+                    // Startup.Configure(configuration, services, enableSwagger: false, enableRabbitMq: true);
                 })
                 .Build();
 
