9bcdb53f9915aa20fbf2a919cd7e359214d55d7e
teamDevsBrad <75026910+teamDevsBrad@users.noreply.github.com>
2024-03-16 15:06:12 -0300

    view inicial para a view de listar videos

--- hackatonposfiap/src/commons/hackatonposfiap.domain/Dtos/GerenciadorVideoItemDto.cs
+++ hackatonposfiap/src/commons/hackatonposfiap.domain/Dtos/GerenciadorVideoItemDto.cs
@@ -6,6 +6,7 @@ public class GerenciadorVideoItemDto
     public string CaminhoArquivo { get; set; }
     public string NomeArquivo { get; set; }
     public string Intervalo { get; set; }
+    public DateTime DtCriacao { get; set; }
 
     public IFormFile? Arquivo { get; set; }
 }
