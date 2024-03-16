e62ebffd2aef1de6bcba9cb5938038234a56e71a
bessax <andrebessax@gmail.com>
2024-03-16 14:30:42 -0300

    #Update:Ajustes

---
+++ hackatonposfiap/src/api/hackatonposfiap.api/Controllers/GerenciadorImagemController.cs
@@ -0,0 +1,51 @@
+﻿using hackatonposfiap.domain.Dtos;
+using hackatonposfiap.domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hackatonposfiap.api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class GerenciadorImagemController : ControllerBase
+{
+ 
+    [HttpPost]
+    public IActionResult CadastrarGerenciadorImagem([FromBody] GerenciadorImagemDto gerenciadorImagemDto)
+    {
+            var gerenciadorImagem = new GerenciadorImagem
+            {
+                ArquivoZip = gerenciadorImagemDto.ArquivoZip,
+                Imagens = gerenciadorImagemDto.Imagens.Select(x => new GerenciadorImagemItem
+                {
+                    CaminhoArquivo = x.CaminhoArquivo,
+                    NomeArquivo = x.NomeArquivo
+                }).ToList()
+            };
+
+           return Ok("Dados válidos");
+        }
+
+
+    [HttpGet]
+    public IActionResult ListarGerenciadoresImagem()
+    {
+        var gerenciadoresImagem = new List<GerenciadorImagem>
+        {
+            new GerenciadorImagem
+            {
+                ArquivoZip = "arquivo.zip",
+                Imagens = new List<GerenciadorImagemItem>
+                {
+                    new GerenciadorImagemItem
+                    {
+                        CaminhoArquivo = "caminho",
+                        NomeArquivo = "nome"
+                    }
+                }
+            }
+        };
+
+        return Ok(gerenciadoresImagem);
+    }
+   
+    
+}
---
+++ hackatonposfiap/tests/hackatonposfiap.integration.test/GerenciadorImagem_POST.cs
@@ -0,0 +1,45 @@
+﻿using hackatonposfiap.domain.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackatonposfiap.integration.test;
+public class GerenciadorImagem_POST:IClassFixture<HackaWebApplicationFactory>
+{
+    private readonly HackaWebApplicationFactory _factory;
+
+    public GerenciadorImagem_POST(HackaWebApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task CadastrarGerenciadorImagem_RetornaSucesso()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var gerenciadorImagemDto = new GerenciadorImagemDto
+        {
+            ArquivoZip = "arquivo.zip",
+            Imagens = new List<GerenciadorImagemItemDto>
+            {
+                new GerenciadorImagemItemDto
+                {
+                    CaminhoArquivo = "caminho",
+                    NomeArquivo = "nome"
+                }
+            }
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync("GerenciadorImagem/", gerenciadorImagemDto);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+    }
+}
